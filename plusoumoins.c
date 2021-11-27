291a458c166083cdc2a9c04aceb6a54f943512ec
patrickkucera <84940200+patrickkucera@users.noreply.github.com>
2021-11-28 00:53:23 +0100

    plusmoins

--- plusoumoins.c
+++ plusoumoins.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minus.c                                            :+:      :+:    :+:   */
+/*   plusoumoins.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: patrickkucera <patrickkucera@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 15:11:09 by pakucera          #+#    #+#             */
-/*   Updated: 2021/11/25 17:18:42 by patrickkuce      ###   ########.fr       */
+/*   Updated: 2021/11/28 00:51:48 by patrickkuce      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,29 @@
 #include <stdlib.h>
 #include <time.h>
 
+int menu()
+{
+    int choix = 0;
+    
+    while (choix < 1 || choix > 4)
+    {
+        printf("Menu :\n");
+        printf("1 : Niveau 1\n");
+        printf("2 : Niveau 2\n");
+        printf("3 : Niveau 3\n");
+        printf("Votre choix ? ");
+        scanf("%d", &choix);
+    }    
+    
+    return choix;
+}    
+
 int main (int argc, char** argv)
 {
     int		NombreMystere = 0, NombreSaisi = 0;
     char	ContinuePartie = 'y';
-	const	int MAX = 100, MIN = 1;
+	int 	MIN = 1;
+	int		NOMBREMAX = 10; 
     
 	// Génération du nombre aléatoire
 
@@ -26,9 +44,21 @@ int main (int argc, char** argv)
     
    do /* La boucle du programme. Elle se répète tant que l'utilisateur n'indique pas qu il souhaite arreter */
    {
+	   switch (menu())
+    {
+        case 1:
+            NOMBREMAX = 3;
+            break;
+        case 2:
+            NOMBREMAX = 6;
+            break;
+        case 3:
+            NOMBREMAX = 9;
+            break;
+    }
 	    if (ContinuePartie == 'y')
 		{
-			NombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+			NombreMystere = (rand() % (NOMBREMAX - MIN + 1)) + MIN;
 			do /* La boucle du programme. Elle se répète tant que l'utilisateur n'a pas trouvé le nombre mystère */
 			{
 			// On demande le nombre
@@ -55,5 +85,3 @@ int main (int argc, char** argv)
 	} while (ContinuePartie != 'n');
    	return (0);
 }
-
- 
