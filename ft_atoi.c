34607d79fe855a19ab90787e8d15296f8440aa3e
pakucera <pakucera@student.42lausanne.ch>
2021-12-22 15:05:24 +0100

    commit

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 14:37:41 by pakucera          #+#    #+#             */
-/*   Updated: 2021/12/22 14:38:11 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/12/22 14:40:41 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,34 @@
 
 static int	ft_isspace(int c)
 {
-	return (c == ' ' || c == '\t' || c == '\n' || c == '\v'
-		|| c == '\f' || c == '\r');
+	return (c == '\f' || c == '\n' || c == '\r' \
+	|| c == '\t' || c == '\v' || c == ' ');
 }
 
 int	ft_atoi(const char *str)
 {
-	int	nb;
-	int	is_negative;
+	long	num;
+	int		sign;
 
-	nb = 0;
-	is_negative = 1;
+	num = 0;
+	sign = 1;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
-		if (*str++ == '-')
-			is_negative = -is_negative;
-	while (*str != '\0' && '0' <= *str && *str <= '9')
 	{
-		nb = *str - '0' + 10 * nb;
+		if (*str == '-')
+			sign = -1;
 		str++;
 	}
-	return (nb * is_negative);
+	while (ft_isdigit(*str))
+	{
+		if ((num > LONG_MAX / 10) || (num == LONG_MAX / 10 && *str > '7'))
+		{
+			if (sign == 1)
+				return (-1);
+			return (0);
+		}
+		num = num * 10 + (*str++ - '0');
+	}
+	return ((int)(sign * num));
 }
